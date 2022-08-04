27a74583cb94b5df9588a5a2b5af99e2740c4439
whsqkaak <whsqkaak@naver.com>
2022-08-04 13:22:19 +0900

    Create RNN Layer and test code.

--- src/rnn.py
+++ src/rnn.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch import Tensor
 
+import copy
 from typing import Optional
 
 
@@ -107,3 +108,109 @@ class RNNCell(nn.Module):
             output = output.squeeze(0)
         
         return output
+    
+
+class RNNLayer(nn.Module):
+    """
+    This class is a implementation of RNN Layer.
+    
+    Args:
+        input_size:
+            The number of expected features in the input `x`.
+        hidden_size:
+            The number of features in the hidden state `h`.
+        num_classes:
+            The number of output classes.
+        bias:
+            If `False`, then the layer does not use bias.
+        activation:
+            The activation function to use. Can be either `tanh` or `relu`.
+            
+    """
+    
+    def __init__(
+        self,
+        input_size: int,
+        hidden_size: int,
+        num_classes: int,
+        bias: bool = True,
+        activation: str = "tanh"
+    ) -> None:
+        super().__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.bias = bias
+        
+        # Stacking RNN layers
+        self.rnn_cell = RNNCell(input_size, hidden_size, bias, activation)
+        
+        # Setting output layer
+        self.output_layer = nn.Linear(hidden_size, num_classes)
+    
+    def forward(
+        self,
+        inputs: Tensor,
+        hidden: Optional[Tensor] = None
+    ) -> Tensor:
+        """
+        Pass inputs through a RNN Layer.
+        
+        Args:
+            inputs:
+                A tensor containing input features.
+            hidden:
+                A tensor containing the initial hidden state.
+                
+        Returns:
+            output:
+                A tensor containing the output features from the last layer of the RNN.
+            h_n:
+                A tensor containing the final hidden state for each element in the batch.
+                    
+        Shape:
+            inputs: `(L, H_{in})` or `(N, L, H_{in})`
+            hidden: `(L, H_{out})` or `(N, L, H_{out})`
+            Returns: 
+                output: `(L, num_classes)` or `(N, L, num_classes)`
+                h_n: `(L, H_{out})` or `(N, L, H_{out})`
+            
+            where
+                N is a batch size.
+                L is a sequnece length.
+                H_{in} is a input size.
+                H_{out} is a hidden size.
+                num_classes is a number of classes.
+        """
+        is_batched = inputs.dim() == 3
+        if not is_batched:
+            # `(L, H_{in})` -> `(1, L, H_{in})`
+            inputs = inputs.unsqueeze(0)
+        len_sequence = inputs.shape[1]
+            
+        if hidden is None:
+            # Create initial hidden state.
+            # `(N, L, H_{out})`
+            hidden = torch.zeros(inputs.shape[0], len_sequence, self.hidden_size, dtype=inputs.dtype)
+        else:
+            # If not batched, `(L, H_{out})` -> `(1, L, H_{out})`
+            hidden = hidden.unsqueeze(0) if not is_batched else hidden
+            
+        h_n = []
+        h_i = hidden[:, 0, :]
+        
+        for i in range(len_sequence):
+            h_i = self.rnn_cell(inputs[:,i,:], h_i)
+            h_n.append(h_i)
+        
+        h_n = torch.stack(h_n, 1) # `(N, L, H_{out})`
+        
+        output = self.output_layer(h_n)
+        
+        if not is_batched:
+            # `(1, L, num_classes)` -> `(L, num_classes)`
+            output = output.squeeze(0)
+            
+            # `(1, L, H_{out})` -> `(L, H_{out})`
+            h_n = h_n.squeeze(0)
+        
+        return output, h_n
---
+++ test/test_RNN.py
@@ -0,0 +1,58 @@
+"""
+Usage:
+    cd RNN; pytest test/test_RNN.py
+"""
+
+import pytest
+import torch
+
+from src.rnn import (
+    RNNCell,
+    RNNLayer
+)
+
+def rnn_cell_test():
+    # Prepare RNNCell instance and dummy data.
+    batch_size = 3
+    input_size = 10
+    hidden_size = 20
+    
+    inputs = torch.randn(batch_size, input_size)
+    hidden = torch.randn(batch_size, hidden_size)
+    
+    rnn_cell = RNNCell(input_size, hidden_size, bias=True, activation="tanh")
+    
+    output = rnn_cell(inputs, hidden)
+    
+    # The shape of output must be `(batch_size, hidden_size)`
+    assert output.shape == (batch_size, hidden_size)
+    
+    
+def rnn_layer_test():
+    # Prepare RNN instance and dummy data.
+    batch_size = 3
+    input_size = 10
+    hidden_size = 20
+    len_sequence = 8
+    num_classes = 8
+    
+    inputs = torch.randn(batch_size, len_sequence, input_size)
+    hidden = torch.randn(batch_size, len_sequence, hidden_size)
+    
+    rnn_layer = RNNLayer(input_size, 
+              hidden_size,
+              num_classes,
+              bias=True,
+              activation="tanh"
+             )
+    
+    output, h_n = rnn_layer(inputs, hidden)
+    
+    # Check the shape of output and h_n
+    assert output.shape == (batch_size, num_classes, num_classes)
+    assert h_n.shape == (batch_size, len_sequence, hidden_size)
+
+
+def test_main():
+    rnn_cell_test()
+    rnn_layer_test()
